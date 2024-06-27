48be03997dbebc9bd81097221e420ce769edcf0c
Bartlomiej Kloc <bartlomiejkloc00@gmail.com>
2024-06-27 12:14:35 +0200

    init

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+    <meta chrser="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <title>Jakaś gierka</title>
+</head>
+<body>
+
+    <form action="login.php" method="post">
+        Login:
+        <input type="text" name="login" />
+        <br/>
+        Password:
+        <input type="password" name="password" />
+        <br/><br/>
+        If you're not register, you can create your account <a href="register.php">here</a>
+        <br/><br/>
+        <input type="submit" value="Log in" />
+    </form>
+</body>
+</html>
